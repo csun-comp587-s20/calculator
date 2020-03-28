a61ab7adaf0d482454fd29b1fc056f62d56a64df
ocasarez <oscar.casarezruiz.491@my.csun.edu>
2020-03-27 22:56:13 -0700

    Created Test Project, CalculatorTestSuite. Setup WinAppDriver and created initial tests for Standard and Scientific mode.

---
+++ src/CalculatorTestSuite/ScientificCalculatorTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+using CalculatorTestSuite.TestFramework;
+using System.Threading;
+
+namespace CalculatorTestSuite
+{
+    [TestFixture]
+    public class ScientificCalculatorTests
+    {
+        private WindowsDriver<WindowsElement> _driver;
+        private StandardCalculatorPage _StandardCalculatorPage;
+        private static WindowsElement header;
+        private static WindowsElement calculatorResult;
+
+        [SetUp]
+        public void TestInit()
+        {
+            var options = new AppiumOptions();
+            options.AddAdditionalCapability("app", "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
+            options.AddAdditionalCapability("deviceName", "WindowsPC");
+            _driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), options);
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            _StandardCalculatorPage = new StandardCalculatorPage(_driver);
+
+            // Identify calculator mode by locating the header
+            try
+            {
+                header = _driver.FindElementByAccessibilityId("Header");
+            }
+            catch
+            {
+                header = _driver.FindElementByAccessibilityId("ContentPresenter");
+            }
+
+            // Ensure that calculator is in scientific mode
+            if (!header.Text.Equals("Scientific", StringComparison.OrdinalIgnoreCase))
+            {
+                _driver.FindElementByAccessibilityId("TogglePaneButton").Click();
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                var menu = _driver.FindElementByClassName("SplitViewPane");
+                menu.FindElementByName("Scientific Calculator").Click();
+                _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
+                Assert.IsTrue(header.Text.Equals("Scientific", StringComparison.OrdinalIgnoreCase));
+            }
+
+            // Locate the calculatorResult element
+            calculatorResult = _driver.FindElementByAccessibilityId("CalculatorResults");
+            Assert.IsNotNull(calculatorResult);
+        }
+
+        [TearDown]
+        public void TestCleanup()
+        {
+            if (_driver != null)
+            {
+                _driver.Quit();
+                _driver = null;
+            }
+        }
+
+        [Test]
+        public void Addition()
+        {
+            _StandardCalculatorPage.Addition(5, 7);
+            _StandardCalculatorPage.EqualsBtn.Click();
+            Assert.AreEqual("12", _StandardCalculatorPage.ReturnCalculatorResults());
+        }
+
+        [Test]
+        public void Division()
+        {
+            _StandardCalculatorPage.Division(8, 1);
+            _StandardCalculatorPage.EqualsBtn.Click();
+            Assert.AreEqual("8", _StandardCalculatorPage.ReturnCalculatorResults());
+        }
+
+        [Test]
+        public void Multiplication()
+        {
+            _StandardCalculatorPage.Multiplication(9, 9);
+            _StandardCalculatorPage.EqualsBtn.Click();
+            Assert.AreEqual("81", _StandardCalculatorPage.ReturnCalculatorResults());
+        }
+
+        [Test]
+        public void Subtraction()
+        {
+            _StandardCalculatorPage.Subtraction(9, 1);
+            _StandardCalculatorPage.EqualsBtn.Click();
+            Assert.AreEqual("8", _StandardCalculatorPage.ReturnCalculatorResults());
+        }
+    }
+}
---
+++ src/CalculatorTestSuite/TestFramework/StandardCalculatorPage.cs
@@ -0,0 +1,108 @@
+﻿using OpenQA.Selenium.Appium.Windows;
+using System;
+
+namespace CalculatorTestSuite.TestFramework
+{
+    public class StandardCalculatorPage
+    {
+        public WindowsDriver<WindowsElement> _session;
+
+        public StandardCalculatorPage(WindowsDriver<WindowsElement> session)
+        {
+            this._session = session;
+        }
+        public void Addition(int value1, int value2)
+        {
+            PressNumber(value1);
+            AdditionBtn.Click();
+            PressNumber(value2);
+        }
+        public void Subtraction(int value1, int value2)
+        {
+            PressNumber(value1);
+            SubtractBtn.Click();
+            PressNumber(value2);
+        }
+        public void Multiplication(int value1, int value2)
+        {
+            PressNumber(value1);
+            MultiplyBtn.Click();
+            PressNumber(value2);
+        }
+        public void Division(int value1, int value2)
+        {
+            PressNumber(value1);
+            DivideBtn.Click();
+            PressNumber(value2);
+        }
+
+        private void PressNumber(int value)
+        {
+            bool negativeFlag = false;
+            if (value < 0)
+            {
+                negativeFlag = true;
+                value = System.Math.Abs(value);
+            }
+            switch (value)
+            {
+                case 0: ZeroBtn.Click(); break;
+                case 1: OneBtn.Click(); break;
+                case 2: TwoBtn.Click(); break;
+                case 3: ThreeBtn.Click(); break;
+                case 4: FourBtn.Click(); break;
+                case 5: FiveBtn.Click(); break;
+                case 6: SixBtn.Click(); break;
+                case 7: SevenBtn.Click(); break;
+                case 8: EightBtn.Click(); break;
+                case 9: NineBtn.Click(); break;
+                default: throw new NotSupportedException($"Value: {value} not supported ");
+            }
+            if (negativeFlag) NegativeBtn.Click();
+        }
+
+        #region Header
+        public WindowsElement WindowHeader => _session.FindElementByName("Standard Calculator mode");
+        public WindowsElement CalculatorMenu => _session.FindElementByName("Open Navigation");
+        #endregion
+
+        #region Number Pad
+        public WindowsElement ZeroBtn => _session.FindElementByName("Zero");
+        public WindowsElement OneBtn => _session.FindElementByName("One");
+        public WindowsElement TwoBtn => _session.FindElementByName("Two");
+        public WindowsElement ThreeBtn => _session.FindElementByName("Three");
+        public WindowsElement FourBtn => _session.FindElementByName("Four");
+        public WindowsElement FiveBtn => _session.FindElementByName("Five");
+        public WindowsElement SixBtn => _session.FindElementByName("Six");
+        public WindowsElement SevenBtn => _session.FindElementByName("Seven");
+        public WindowsElement EightBtn => _session.FindElementByName("Eight");
+        public WindowsElement NineBtn => _session.FindElementByName("Nine");
+        public WindowsElement DecimalBtn => _session.FindElementByName("Decimal Separator");
+        public WindowsElement AdditionBtn => _session.FindElementByName("Plus");
+        public WindowsElement SubtractBtn => _session.FindElementByName("Minus");
+        public WindowsElement MultiplyBtn => _session.FindElementByName("Multiply by");
+        public WindowsElement DivideBtn => _session.FindElementByName("Divide by");
+        public WindowsElement EqualsBtn => _session.FindElementByName("Equals");
+        public WindowsElement NegativeBtn => _session.FindElementByName("Positive Negative");
+        public WindowsElement BackspaceBtn => _session.FindElementByName("Backspace");
+        public WindowsElement ClearBtn => _session.FindElementByName("Clear");
+        public WindowsElement ClearEntryBtn => _session.FindElementByName("Clear entry");
+        public WindowsElement PercentBtn => _session.FindElementByName("Percent");
+        public WindowsElement ReciprocalBtn => _session.FindElementByName("Reciprocal");
+        public WindowsElement SquareBtn => _session.FindElementByName("Square");
+        public WindowsElement SquareRootBtn => _session.FindElementByName("Square root");
+        #endregion
+
+        #region Memory Buttons
+        public WindowsElement MemAddBtn => _session.FindElementByName("Memory Add");
+        public WindowsElement MemRemoveBtn => _session.FindElementByName("Memory Subtract");
+        public WindowsElement MemStoreBtn => _session.FindElementByName("Memory Store");
+        #endregion
+
+        #region Calculator Results
+        public WindowsElement ResultsOutput => _session.FindElementByAccessibilityId("CalculatorResults");
+        #endregion
+
+        public string ReturnCalculatorResults() => ResultsOutput.Text.Replace("Display is", string.Empty).Trim();
+    }
+}
